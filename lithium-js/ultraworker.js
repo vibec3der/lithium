0e3ceb704c7a5def903c00e26180ce078ab8fdfc
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-11-17 20:23:02 -0800

    Update ultraworker.js

--- lithium-js/ultraworker.js
+++ lithium-js/ultraworker.js
@@ -1,30 +1,25 @@
 importScripts("/scram/scramjet.all.js");
 
-
-
 if (navigator.userAgent.includes("Firefox")) {
-  Object.defineProperty(globalThis, "crossOriginIsolated", {
-    value: true,
-    writable: true,
-  })
+	Object.defineProperty(globalThis, "crossOriginIsolated", {
+		value: true,
+		writable: true,
+	});
 }
 
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
-self.addEventListener("install", () => {
-  self.skipWaiting()
-})
-
 async function handleRequest(event) {
-  await scramjet.loadConfig()
-  if (scramjet.route(event)) 
-    return scramjet.fetch(event)
-  
-  
-  return await fetch(event.request)
+	await scramjet.loadConfig();
+
+	if (scramjet.route(event)) {
+		return scramjet.fetch(event);
+	}
+
+	return await fetch(event.request);
 }
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event))
-})
-
+	event.respondWith(handleRequest(event));
+});
